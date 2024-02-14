7abc2a1ba358185f0c70b15f33d320b2f6bf1093
WillBoudreau <williamboudreau804@gmail.com>
2024-02-14 15:25:45 -0500

    Character stops in direction of last key press

--- Assets/Scripts/ChardterController2D.cs
+++ Assets/Scripts/ChardterController2D.cs
@@ -12,8 +12,10 @@ public class ChardterController2D : MonoBehaviour
     [SerializeField] private Animator _anim;
     private float x;
     private float y;
+    private float i;
 
     Vector2 movement;
+    Vector2 lastDirect = Vector2.down;
     void Update()
     {
         _anim.GetComponent<Animation>();
@@ -22,6 +24,30 @@ public class ChardterController2D : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
         x = movement.x;
         y = movement.y;
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("S pressed");
+            _anim.SetFloat("IdleY",1);
+            _anim.SetFloat("IdleX",0);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W pressed");
+           _anim.SetFloat("IdleY",-1);
+           _anim.SetFloat("IdleX",0);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A pressed");
+            _anim.SetFloat("IdleY", 0);
+            _anim.SetFloat("IdleX",1);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("D pressed");
+            _anim.SetFloat("IdleY",0);
+            _anim.SetFloat("IdleX",-1);
+        }
     }
     void FixedUpdate()
     {
@@ -39,7 +65,6 @@ public class ChardterController2D : MonoBehaviour
     {
         _anim.SetFloat("POSX",x);
         _anim.SetFloat("POSY",y);
-
         if(movement != Vector2.zero)
         {
             _anim.SetBool("PlayerPOSMove",true);
